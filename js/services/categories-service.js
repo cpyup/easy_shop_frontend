6b946635639691e4df30efe094d6e52e769af5d9
Cameron <cpolly@pgh.yearup.org>
2024-12-18 09:33:22 -0500

    Fixing Premature Category Error

--- js/services/categories-service.js
+++ js/services/categories-service.js
@@ -2,22 +2,38 @@ let categoryService;
 
 class CategoryService {
 
-
     getAllCategories(callback)
     {
         const url = `${config.baseUrl}/categories`;
 
-        return axios.get(url)
-            .then(response => {
-                callback(response.data);
-            })
-            .catch(error => {
+        let isLoaded = false;
 
+        // Set a timeout that will check for success after a delay to prevent premature error
+        const timeout = setTimeout(() => {
+            if (!isLoaded) {
                 const data = {
                     error: "Loading categories failed."
                 };
+                templateBuilder.append("error", data, "errors");
+            }
+        }, 3000);
 
-                templateBuilder.append("error", data, "errors")
+        return axios.get(url)
+            .then(response => {
+                // If the request is successful, stop the timeout
+                clearTimeout(timeout);
+
+                isLoaded = true;
+                callback(response.data);
+            })
+            .catch(error => {
+                // If there was an error, show the error message after the timeout
+                if (!isLoaded) {
+                    const data = {
+                        error: "Loading categories failed."
+                    };
+                    templateBuilder.append("error", data, "errors");
+                }
             });
     }
 }
