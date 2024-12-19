9b12be778333f7701d1864c65357bb26c1799fdf
Cameron <cpolly@pgh.yearup.org>
2024-12-19 09:25:28 -0500

    Rolled Back Category Fetching

--- js/services/categories-service.js
+++ js/services/categories-service.js
@@ -3,39 +3,19 @@ let categoryService;
 class CategoryService {
 
     getAllCategories(callback)
-    {
-        const url = `${config.baseUrl}/categories`;
-
-        let isLoaded = false;
-
-        // Set a timeout that will check for success after a delay to prevent premature error
-        const timeout = setTimeout(() => {
-            if (!isLoaded) {
-                const data = {
-                    error: "Loading categories failed."
-                };
-                templateBuilder.append("error", data, "errors");
-            }
-        }, 500);
-
-        return axios.get(url)
-            .then(response => {
-                // If the request is successful, stop the timeout
-                clearTimeout(timeout);
-
-                isLoaded = true;
-                callback(response.data);
-            })
-            .catch(error => {
-                // If there was an error, show the error message after the timeout
-                if (!isLoaded) {
+        {
+            const url = `${config.baseUrl}/categories`;
+            return axios.get(url)
+                .then(response => {
+                    callback(response.data);
+                })
+                .catch(error => {
                     const data = {
                         error: "Loading categories failed."
                     };
-                    templateBuilder.append("error", data, "errors");
-                }
-            });
-    }
+                    templateBuilder.append("error", data, "errors")
+                });
+        }
 
     // Used to load the selected category to the editing form
         editCategory(categoryId)
